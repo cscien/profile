b3ca2c19e2743834b1d977fae2e089f58cc91c16
cscien <cscien@163.com>
2020-03-31 09:31:42 +0800

    添加照片和简历
    通过static files 实现 ？？？

--- profiles/settings.py
+++ profiles/settings.py
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'profiles/static_files'),]
+
+STATIC_URL = '/static_files/'
+
+STATIC_ROOT =os.path.join(BASE_DIR, 'static_files')
 
 MEDIA_URL = '/media/'
 
